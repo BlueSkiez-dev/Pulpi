3a8b6f101d23c171511bfcacaaa8c3eff136585f
My Name <my.adresse@hotmail.com>
2021-01-15 15:30:13 +0100

    ...

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -2,7 +2,9 @@ import 'package:flutter/painting.dart';
 
 class Palette {
   static const Color darkBlue = Color(0xff092E34);
-  static const Color lightBlue = Color(0xff489FB5);
-  static const Color orange = Color(0xffFFA62B);
+  static const Color lightBlue = Color(0xffFAE7CB);
+  static const Color orange = Color(0xffFFB385);
   static const Color darkOrange = Color(0xffCC7700);
+  static const Color brightPink = Color(0xffFF7272);
+  static const Color yellowish = Color(0xffFFD31D);
 }
--- lib/screens/background_painter.dart
+++ lib/screens/background_painter.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pulpi/config/palette.dart';
 
 class BackgroundPainter extends CustomPainter {
+  BackgroundPainter()
+      : bluePaint = Paint()
+          ..color = Palette.darkBlue
+          ..style = PaintingStyle.fill;
+
+  final Paint bluePaint;
+
   @override
   void paint(Canvas canvas, Size size) {
+    print('painting');
+    canvas.drawPaint(Paint()..color = Palette.darkBlue);
     // TODO- IMPLEMENT PAINT
   }
 
