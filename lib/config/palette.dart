3cd354450fe2d1c5c0937e8e40c9768033d06e2e
My Name <my.adresse@hotmail.com>
2021-01-15 15:34:48 +0100

    ...

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -2,7 +2,7 @@ import 'package:flutter/painting.dart';
 
 class Palette {
   static const Color darkBlue = Color(0xff092E34);
-  static const Color lightBlue = Color(0xffFAE7CB);
+  static const Color lightcream = Color(0xffFAE7CB);
   static const Color orange = Color(0xffFFB385);
   static const Color darkOrange = Color(0xffCC7700);
   static const Color brightPink = Color(0xffFF7272);
--- lib/screens/background_painter.dart
+++ lib/screens/background_painter.dart
@@ -6,9 +6,17 @@ class BackgroundPainter extends CustomPainter {
   BackgroundPainter()
       : bluePaint = Paint()
           ..color = Palette.darkBlue
+          ..style = PaintingStyle.fill,
+        creamPaint = Paint()
+          ..color = Palette.lightcream
+          ..style = PaintingStyle.fill,
+        orangePaint = Paint()
+          ..color = Palette.orange
           ..style = PaintingStyle.fill;
 
   final Paint bluePaint;
+  final Paint creamPaint;
+  final Paint orangePaint;
 
   @override
   void paint(Canvas canvas, Size size) {
